d0c356a20d8d7c6ff8663997de2cf9ecbe84c910
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2024-10-11 13:41:53 +0200

    feat : interactive work on buttons and hearts

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
+let hearthDeathPoints = 10;
+let hearthCharacterPoints = 3;
+
 function checkAnswer(answer) {
     const buttons = document.querySelectorAll(".buttonResponse");
+    const deathHearts = document.querySelectorAll(".blackHeart");
+    const characterHearts = document.querySelectorAll(".redHeart");
     
     buttons.forEach(button => {
             button.disabled = true;
 
         if (button.textContent === answer) {
             if (answer === "8") {
-                button.style.backgroundColor = 'green'; 
+                button.style.backgroundColor = 'green';    
             } 
             if (answer !== "8") {
                 button.style.backgroundColor = 'red'; 
+                if (hearthCharacterPoints>0){
+                    characterHearts[hearthCharacterPoints - 1].src = "./Public/Images/heart-black-breack.png";
+                hearthCharacterPoints--;
+                }
             }
         } else {
             if (button.textContent !== "8") {
@@ -17,4 +26,9 @@ function checkAnswer(answer) {
             }
         }
     });
+    
+    if (hearthDeathPoints>0){
+        deathHearts[hearthDeathPoints - 1].src = "./Public/Images/heart-black-breack.png";
+    hearthDeathPoints--;
+    }
 }
