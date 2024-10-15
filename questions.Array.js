bd7d742441e8219497ee7014a67366907765cd58
Sanesthesie <victorsanlopez@gmail.com>
2024-10-15 14:56:29 +0200

    feat : correction of an error in the answers

--- questions.Array.js
+++ questions.Array.js
@@ -1,120 +1,121 @@
 let quizList = [
+	{
+		question: "Combien de planètes orbittent autour du soleil ? ",
+		clue: "On parle bien de planètes !",
+		answer1: ["12", false],
+		answer2: ["9", false],
+		answer3: ["8", true],
+		answer4: ["6", false],
+	},
 
-    {
-        question : "Combien de planètes orbittent autour du soleil ? ",
-        clue : "On parle bien de planètes !",
-        answer1 : ["12", false],
-        answer2 : ["9", false],
-        answer3 : ["8", true],
-        answer4 : ["6", false]
-    },
+	{
+		question:
+			"Laquelle de ces villes n'a jamais été la capitale de la France ?",
+		clue: "L'Histoire est écrite par les vainqueurs...",
+		answer1: ["Bordeaux", false],
+		answer2: ["Tournai", false],
+		answer3: ["Avignon", true],
+		answer4: ["Brazzaville", false],
+	},
 
-    {
-        question : "Laquelle de ces villes n'a jamais été la capitale de la France ?",
-        clue : "L'Histoire est écrite par les vainqueurs...",
-        answer1 : ["Bordeaux", false],
-        answer2 : ["Tournai", false],
-        answer3 : ["Avignon", true],
-        answer4 : ["Brazzaville", false]
-    },
+	{
+		question: "Quel est l'animal national de l'Écosse ?",
+		clue: "Un animal national est choisi par une communauté comme son emblème.",
+		answer1: ["Le lion", false],
+		answer2: ["Le dragon", false],
+		answer3: ["Le bélier", false],
+		answer4: ["La licorne", true],
+	},
 
-    {
-        question : "Quel est l'animal national de l'Écosse ?",
-        clue : "Un animal national est choisi par une communauté comme son emblème.",
-        answer1 : ["Le lion", false],
-        answer2 : ["Le dragon", false],
-        answer3 : ["Le bélier", false],
-        answer4 : ["La licorne", true]
-    },
+	{
+		question: "Quel réalisateur a créé le célèbre personnage d'Indiana Jones ?",
+		clue: "Trop facile !",
+		answer1: ["Georges Lucas", true],
+		answer2: ["Steven Spielberg", false],
+		answer3: ["Quentin Tarantino", false],
+		answer4: ["James Cameron", false],
+	},
 
-    {
-        question : "Quel réalisateur a créé le célèbre personnage de Indiana Jones ?",
-        clue : "Trop facile !",
-        answer1 : ["Georges Lucas", false],
-        answer2 : ["Steven Spielberg", false],
-        answer3 : ["Quentin Tarantino", false],
-        answer4 : ["James Cameron", true]
-    },
+	{
+		question: "Qui a créé en dix jours le langage JavaScript ?",
+		clue: "On n'est pas encore à son niveau par ici...",
+		answer1: ["Brendan Eich", true],
+		answer2: ["Bryan Eisch", false],
+		answer3: ["Brenton Eickhoff", false],
+		answer4: ["Benjamin Eichel", false],
+	},
 
-    {
-        question : "Qui a créé en dix jours le langage JavaScript ?",
-        clue : "On n'est pas encore à son niveau par ici...",
-        answer1 : ["Brendan Eich", true],
-        answer2 : ["Bryan Eisch", false],
-        answer3 : ["Brenton Eickhoff", false],
-        answer4 : ["Benjamin Eichel", false]
-    },
+	{
+		question: "Quelle est la date d'anniversaire de mon ami Voldemort ?",
+		clue: "On a vu qu'il y avait des potterhead par ici...",
+		answer1: ["1er avril", false],
+		answer2: ["31 décembre", true],
+		answer3: ["30 octobre", false],
+		answer4: ["29 février", false],
+	},
 
-    {
-        question : "Quelle est la date d'anniversaire de mon ami Voldemort ?",
-        clue : "On a vu qu'il y avait des potterhead par ici...",
-        answer1 : ["1er avril", false],
-        answer2 : ["31 décembre", true],
-        answer3 : ["30 octobre", false],
-        answer4 : ["29 février", false]
-    },
+	{
+		question:
+			"Lequel des ces types de données en Java est utilisé pour stocker un seul caractère ?",
+		clue: "Vous avez bien dit Java ?",
+		answer1: ["float", false],
+		answer2: ["char", true],
+		answer3: ["byte", false],
+		answer4: ["int", false],
+	},
 
-    {
-        question : "Lequel des ces types de données en Java est utilisé pour stocker un seul caractère ?",
-        clue : "Vous avez bien dit Java ?",
-        answer1 : ["float", false],
-        answer2 : ["char", true],
-        answer3 : ["byte", false],
-        answer4 : ["int", false]
-    },
+	{
+		question: "Quel est le plus grand pays d'Afrique ?",
+		clue: "En termes de superficie bien entendu !",
+		answer1: ["La Turquie", false],
+		answer2: ["Le Mali", false],
+		answer3: ["Le Soudan", false],
+		answer4: ["l'Algérie", true],
+	},
 
-    {
-        question : "Quel est le plus grand pays d'Afrique ?",
-        clue : "En termes de superficie bien entendu !",
-        answer1 : ["La Turquie", false],
-        answer2 : ["Le Mali", false],
-        answer3 : ["Le Soudan", false],
-        answer4 : ["l'Algérie", true]
-    },
+	{
+		question: "Quel est le poids réglementaire d'un ballon de football ?",
+		clue: "Selon les règles de la Fifa pour une taille 5.",
+		answer1: ["300-320 grammes", false],
+		answer2: ["350-370 grammes", false],
+		answer3: ["450-470 grammes", false],
+		answer4: ["400-420 grammes", true],
+	},
 
-    {
-        question : "Quel est le poids réglementaire d'un ballon de football ?",
-        clue : "Selon les règles de la Fifa pour une taille 5.",
-        answer1 : ["300-320 grammes", false],
-        answer2 : ["350-370 grammes", false],
-        answer3 : ["450-470 grammes", false],
-        answer4 : ["400-420 grammes", true],
-    },
+	{
+		question: "Quelle est la capitale de l'Australie ?",
+		clue: "Allez, tout le monde la connaît celle là !",
+		answer1: ["Sydney", false],
+		answer2: ["Melbourne", false],
+		answer3: ["Canberra", true],
+		answer4: ["Brisbane", false],
+	},
 
-    {
-        question : "Quelle est la capitale de l'Australie ?",
-        clue : "Allez, tout le monde la connaît celle là !",
-        answer1 : ["Sydney", false],
-        answer2 : ["Melbourne", false],
-        answer3 : ["Canberra", true],
-        answer4 : ["Brisbane", false],
-    },
+	{
+		question:
+			"Quel est le nom du personnages principal dans le roman 1984 de George Orwell ?",
+		clue: "J'espère que tout le monde a lu ce bouquin !",
+		answer1: ["Winston Churchill", false],
+		answer2: ["Winston Smith", true],
+		answer3: ["Winston Duke", false],
+		answer4: ["Winston Bogarde", false],
+	},
 
-    {
-        question : "Quel est le nom du personnages principal dans le roman 1984 de George Orwell ?",
-        clue : "J'espère que tout le monde a lu ce bouquin !",
-        answer1 : ["Winston Churchill", false],
-        answer2 : ["Winston Smith", true],
-        answer3 : ["Winston Duke", false],
-        answer4 : ["Winston Bogarde", false]
-    },
+	{
+		question: "Quelle est la langue la plus parlée dans le monde ?",
+		clue: "Ne jouons pas avec les mots...",
+		answer1: ["L'anglais", false],
+		answer2: ["L'arabe", false],
+		answer3: ["L'espagnol", false],
+		answer4: ["le mandarin", true],
+	},
 
-    {
-        question : "Quelle est la langue la plus parlée dans le monde ?",
-        clue : "Ne jouons pas avec les mots...",
-        answer1 : ["L'anglais", false],
-        answer2 : ["L'arabe", false],
-        answer3 : ["L'espagnol", false],
-        answer4 : ["le mandarin", true]
-    },
-
-    {
-        question : "Quel est le symbole chimique de l'or ?",
-        clue : "C'est loin les cours de physique ?",
-        answer1 : ["Au", true],
-        answer2 : ["Mo", false],
-        answer3 : ["Go", false],
-        answer4 : ["Or", false]
-    },
+	{
+		question: "Quel est le symbole chimique de l'or ?",
+		clue: "C'est loin les cours de physique ?",
+		answer1: ["Au", true],
+		answer2: ["Mo", false],
+		answer3: ["Go", false],
+		answer4: ["Or", false],
+	},
 ];
-
