57d6b4645da27b85e4db1136862d43a540a64406
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2024-10-17 16:56:20 +0200

    feat : created popup

--- script.js
+++ script.js
@@ -127,6 +127,18 @@ let currentQuestionIndex = 0;
 let heartsCount = 13;
 let redheartsCount = 3;
 
+window.onload = function () {
+	document.getElementById("popup").style.display = "flex";
+};
+
+document.getElementById("close-popup").addEventListener("click", () => {
+	document.getElementById("popup").style.display = "none";
+	document.getElementsByClassName("bubbleQuestion").style.display = "flex";
+	loadQuestion(questions[currentQuestionIndex]);
+});
+
+///////////////////////////////////////////////////////////////
+
 function loadQuestion() {
 	const questionElement = document.getElementById("question");
 	const clueElement = document.getElementById("clue");
